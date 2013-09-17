e7e28abdb63ca1b6455bf9f4cbc0fd288429b217
Sameer Dhar <avielus@gmail.com>
2013-09-17 16:11:12 -0400

    fixed user signup for real

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  def new
+    @user = User.new
+  end
   def create
     @user = User.new(user_params)
     if @user.save
