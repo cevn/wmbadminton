c848efe8b6ef22e9d3d88af936b0d33ad1440f11
Sameer Dhar <avielus@gmail.com>
2013-09-17 16:59:08 -0400

    working user authentication for real

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to railswolf!"
       redirect_to @user
     else
