f8f28826c65e9eea3293641ce0e1db1de914983c
Sameer Dhar <avielus@gmail.com>
2013-09-17 16:00:56 -0400

    got user authentication working??

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 SdharRailswolf::Application.routes.draw do
-  get "users/new"
-  get 'users/new'
   root 'static_pages#home'
 
+  resources :users
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
