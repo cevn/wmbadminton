e43b485edf977a0c3a083f056d457066d165be4a
Sameer Dhar <avielus@gmail.com>
2013-09-17 11:11:21 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SdharRailswolf::Application.config.secret_key_base = '079d1e9bd4f9c2b0d42148c024df47258fe6383a20feb07c9a1912e6e14a1852667d29e5213bd62159d685edc2e582b0fb73648da44f5561e731dbc980f34e37'
