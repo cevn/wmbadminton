3956b7f6f81eedabb746ddf363e605ec5b502527
Sameer Dhar <avielus@gmail.com>
2013-09-23 11:40:19 -0400

    remove popup on map cuz it looks bad

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,7 +7,7 @@ module UsersHelper
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 
-  def gravatar_for(user, options = { size: 50 })
+  def gravatar_for(user, options = { size: 150})
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
